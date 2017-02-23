0fcb4a25867c1587ce9c8e57dcb2a3521bbf7a25
Donnie Ellstrom <dsvene@gmail.com>
2017-02-23 11:23:57 -0800

    Hide env variables for heroku

--- djangotwitter/views.py
+++ djangotwitter/views.py
@@ -3,15 +3,13 @@ import subprocess
 import sys
 import djangotwitter.singleton
 
-
-
 # Create your views here.
 def index(request):
     return render(request, 'djangotwitter/index.html')
 
 def startbot(reqest):
     #djangotwitter.singleton.proc = subprocess.Popen([sys.executable, '/home/donne/WDI/Week 12/djangotwittersite/twitterBot.py'])
-    djangotwitter.singleton.proc = subprocess.Popen([sys.executable, '../twitterBot.py'])
+    djangotwitter.singleton.proc = subprocess.Popen([sys.executable, 'twitterBot.py'])
     # print(djangotwitter.singleton.proc)
     return redirect('index')
 
