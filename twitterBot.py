831538e7da7938850aa41e39aa25332d6de66dde
Donnie Ellstrom <dsvene@gmail.com>
2017-02-23 09:50:48 -0800

    Fix issue with env variables

--- twitterBot.py
+++ twitterBot.py
@@ -3,6 +3,7 @@ import threading
 import markovify
 import nltk
 import re
+import os
 from time import sleep
 from tweepy.streaming import StreamListener
 from tweepy import Stream
@@ -34,8 +35,8 @@ class TweetBot:
         self.load_botfood(botfood)
 
         #initialize Twitter authorization with tweepy
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
+        auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_TOKEN_SECRET"))
         self.api = tweepy.API(auth)
 
     def load_botfood(self, botfood):
