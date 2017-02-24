e54f80f7a11c3384ddaa6e2b284b3c6ba888af66
Donnie Ellstrom <dsvene@gmail.com>
2017-02-23 19:22:12 -0800

    Add Logged model to admin

--- djangotwitter/admin.py
+++ djangotwitter/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TextFood
+from .models import TextFood, Logged
 # Register your models here.
 
 admin.site.register(TextFood)
+admin.site.register(Logged)
